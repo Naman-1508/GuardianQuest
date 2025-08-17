27fdfa2c0caf9efcc8ec919663b4ed54bc4cb420
Naman Jain <namanjain01508@gmail.com>
2025-08-17 21:12:01 +0530

    feat: add password hashing & jwt methods in user schema

--- src/models/User.models.js
+++ src/models/User.models.js
@@ -1,31 +1,74 @@
-import mongoose from "mongoose"
+    import mongoose from "mongoose";
+    import bcrypt from "bcrypt";
+    import jwt from "jsonwebtoken"; 
+    import { use } from "react";
 
-const userSchema = new mongoose.Schema({
-    username:{
-        type: String,
-        required: true,
-        unique:true,
-        lowercase:true,
-        trim:true
-    },
-    email:{
-        type: String,
-        required: true,
-        unique:true,
-        lowercase:true,
-        trim:true,
-        match:[/^\S+@\S+\.\s+$/,"Please enter a valid email"]
-    },
-    Contact_NO:{
-        type:String,
-        required:true,
-        unique:true,
-        match:[/^\d{10}$/,"Phone number must be exactly 10 digits"]
-        
-    },
-    password:{
-        type:String,
-        required:true,
+
+    const userSchema = new mongoose.Schema({
+        username:{
+            type: String,
+            required: true,
+            unique:true,
+            lowercase:true,
+            trim:true
+        },
+        email:{
+            type: String,
+            required: true,
+            unique:true,
+            lowercase:true,
+            trim:true,
+            match:[/^\S+@\S+\.\S+$/,"Please enter a valid email"]
+        },
+        fullname:{
+            type:String,
+            required:true,
+            trim:true,
+            index:true
+        },
+        contactNo:{
+            type:String,
+            required:true,
+            unique:true,
+            match:[/^\d{10}$/,"Phone number must be exactly 10 digits"]
+            
+        },
+        password:{
+            type:String,
+            required:true,
+        }
+    },{timestamps: true})
+
+    userSchema.pre("save",async function (next) {
+        if(!this.isModified("password")) return next();
+        this.password = await bcrypt.hash(this.password,10)
+        next()
+    })
+
+    userSchema.methods.isPasswordCorrect = async function(password){
+        return await bcrypt.compare(password,this.password)
     }
-},{timestamps: true})
-export const User = mongoose.model("User",userSchema)
+    userSchema.methods.generateAccessToken = function(){
+        return jwt.sign({
+            _id : this._id,
+            email:this.email,
+            username:this.username,
+            fullname:this.fullname,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+    }
+    userSchema.methods.generateRefreshToken = function(){
+        return jwt.sign({
+            _id : this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+    }
+    export const User = mongoose.model("User",userSchema)
